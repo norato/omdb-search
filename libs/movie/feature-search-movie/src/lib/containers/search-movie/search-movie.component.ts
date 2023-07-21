1c8f155dbf3adf6aa758076a47e82013118c09c5
Felipe Norato <fnoratol@gmail.com>
2023-07-21 17:50:37 +0100

    chore: add hero

--- libs/movie/feature-search-movie/src/lib/containers/search-movie/search-movie.component.ts
+++ libs/movie/feature-search-movie/src/lib/containers/search-movie/search-movie.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HeroComponent } from '@omdb-search/common-ui';
 
 @Component({
   selector: 'omdb-search-search-movie',
   standalone: true,
+  imports: [HeroComponent],
   templateUrl: './search-movie.component.html',
   styleUrls: ['./search-movie.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
