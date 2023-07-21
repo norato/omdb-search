93e12002ce5bb1e95df34b109de3b30d2eb072a8
Felipe Norato <fnoratol@gmail.com>
2023-07-21 12:13:11 +0100

    chore: install icons lib

---
+++ libs/common-ui/src/lib/components/star-rating/star-rating.component.stories.ts
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from '@storybook/angular';
+import { StarRatingComponent } from './star-rating.component';
+
+export default {
+  title: 'Components / StarRating',
+  component: StarRatingComponent,
+} as Meta;
+
+type Story = StoryObj<StarRatingComponent>;
+
+export const StarRating: Story = {
+  render: () => ({
+    props: {},
+  }),
+};
--- libs/common-ui/src/lib/components/star-rating/star-rating.component.ts
+++ libs/common-ui/src/lib/components/star-rating/star-rating.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faStar as starRegular } from '@fortawesome/free-regular-svg-icons';
+import { faStar as starSolid } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'omdb-search-star-rating',
   standalone: true,
+  imports: [FontAwesomeModule],
   templateUrl: './star-rating.component.html',
   styleUrls: ['./star-rating.component.scss'],
 })
-export class StarRatingComponent {}
+export class StarRatingComponent {
+  @Input() rating?: string;
+
+  faStarSolid = starSolid;
+  faStarRegular = starRegular;
+}
