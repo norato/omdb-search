fd95132f75d8d409a647d6de580a764fd57d89d4
Felipe Norato <fnoratol@gmail.com>
2023-07-21 13:59:12 +0100

    chore: no modules are required

--- libs/common-ui/src/index.ts
+++ libs/common-ui/src/index.ts
@@ -1,1 +1,1 @@
-export * from './lib/common-ui.module';
+export * from './lib';
---
+++ libs/common-ui/src/lib/components/index.ts
@@ -0,0 +1,3 @@
+export * from './header/header.component';
+export * from './hero/hero.component';
+export * from './star-rating/star-rating.component';
